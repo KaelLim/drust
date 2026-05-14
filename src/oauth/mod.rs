c0838c98f8ba76f96ebde35667b67f8a9d61d2d3
KaelLim <kaelsohappy1@gmail.com>
2026-05-14 22:33:23 +0800

    oauth: ProviderRegistry::from_env + allowlist parser

--- src/oauth/mod.rs
+++ src/oauth/mod.rs
@@ -1,6 +1,8 @@
+pub mod config;
 pub mod github;
 pub mod google;
 pub mod provider;
 pub mod state;
 
+pub use config::ProviderRegistry;
 pub use provider::{OauthError, OauthProvider, VerifiedUser};
